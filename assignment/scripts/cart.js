40369db18ba437e5f00d02588f5eb0ffd9d8dd0f
Abdi <alldayabdi@gmail.com>
2022-01-10 09:43:01 -0600

    Finished assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,22 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket =[];
+function addItem (item) {
+console.log('in addItem',);
+basket.push (item);
+return true;
+
+}
+console.log('Return true if item is added', addItem('apples'));
+
+function listItem() {
+  console.log(basket);
+}
+listItem();
+
+function empty(basket) {
+  basket = [];
+
+}
+console.log(empty());
